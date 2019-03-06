ac9ac502a9b0fb159f303f6e49d0d3d306ed65ea
Daniël de Kok <me@danieldk.eu>
2019-03-06 08:16:25 +0100

    Add an example of getting metadata

---
+++ examples/print-metadata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+from finalfusion import Embeddings
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s embeddings\n" % sys.argv[0])
+        sys.exit(1)
+
+    embeds = Embeddings(sys.argv[1])
+
+    print(embeds.metadata)
