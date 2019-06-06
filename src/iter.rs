488e1b1b4856cea08aeb9a3eacb1e74f9a12d00b
Daniël de Kok <me@danieldk.eu>
2019-06-06 10:34:08 +0200

    Split lib.rs into several modules

---
+++ src/iter.rs
@@ -0,0 +1,50 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use finalfusion::prelude::*;
+use numpy::{IntoPyArray, PyArray1};
+use pyo3::class::iter::PyIterProtocol;
+use pyo3::prelude::*;
+
+use crate::EmbeddingsWrap;
+
+#[pyclass(name=EmbeddingIterator)]
+pub struct PyEmbeddingIterator {
+    embeddings: Rc<RefCell<EmbeddingsWrap>>,
+    idx: usize,
+}
+
+impl PyEmbeddingIterator {
+    pub fn new(embeddings: Rc<RefCell<EmbeddingsWrap>>, idx: usize) -> Self {
+        PyEmbeddingIterator { embeddings, idx }
+    }
+}
+
+#[pyproto]
+impl PyIterProtocol for PyEmbeddingIterator {
+    fn __iter__(slf: PyRefMut<Self>) -> PyResult<Py<PyEmbeddingIterator>> {
+        Ok(slf.into())
+    }
+
+    fn __next__(mut slf: PyRefMut<Self>) -> PyResult<Option<(String, Py<PyArray1<f32>>)>> {
+        let slf = &mut *slf;
+
+        let embeddings = slf.embeddings.borrow();
+        let vocab = embeddings.vocab();
+
+        if slf.idx < vocab.len() {
+            let word = vocab.words()[slf.idx].to_string();
+            let embed = embeddings.storage().embedding(slf.idx);
+
+            slf.idx += 1;
+
+            let gil = pyo3::Python::acquire_gil();
+            Ok(Some((
+                word,
+                embed.into_owned().into_pyarray(gil.python()).to_owned(),
+            )))
+        } else {
+            Ok(None)
+        }
+    }
+}
---
+++ src/similarity.rs
@@ -0,0 +1,35 @@
+use pyo3::class::basic::PyObjectProtocol;
+use pyo3::prelude::*;
+
+/// A word and its similarity to a query word.
+///
+/// The similarity is normally a value between -1 (opposite
+/// vectors) and 1 (identical vectors).
+#[pyclass(name=WordSimilarity)]
+pub struct PyWordSimilarity {
+    #[pyo3(get)]
+    word: String,
+
+    #[pyo3(get)]
+    similarity: f32,
+}
+
+impl PyWordSimilarity {
+    pub fn new(word: String, similarity: f32) -> Self {
+        PyWordSimilarity { word, similarity }
+    }
+}
+
+#[pyproto]
+impl PyObjectProtocol for PyWordSimilarity {
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "WordSimilarity('{}', {})",
+            self.word, self.similarity
+        ))
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("{}: {}", self.word, self.similarity))
+    }
+}
