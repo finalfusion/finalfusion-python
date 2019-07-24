1d8eb8c4f44f5af56c0fed015b900579d433cf4f
Daniël de Kok <me@danieldk.eu>
2019-07-24 14:48:39 +0200

    Cleanup unit tests
    - Use fixtures to remove repeated code.
    - Replace some incorrect try-except blocks by pytest.raises.
    - Miscellaneous cleaning.

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,9 +1,3 @@
-import finalfusion
-import pytest
-
-
-def test_embeddings_with_norms_oov():
-    embeds = finalfusion.Embeddings(
-        "tests/embeddings.fifu")
-    vocab = embeds.vocab()
+def test_embeddings_with_norms_oov(embeddings_fifu):
+    vocab = embeddings_fifu.vocab()
     assert vocab.item_to_indices("Something out of vocabulary") is None
