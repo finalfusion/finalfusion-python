b27c5d9f7f42f8469e1c92f1fb64c5a081dde074
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-07-27 11:38:29 +0200

    Add embedding similarity queries.
    Add a method to perform similarity queries based on an input
    embedding rather than words.

--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -1,3 +1,6 @@
+import pytest
+import numpy
+
 SIMILARITY_ORDER_STUTTGART_10 = [
     "Karlsruhe",
     "Mannheim",
@@ -57,10 +60,28 @@ SIMILARITY_ORDER = [
 
 
 def test_similarity_berlin_40(similarity_fifu):
-    for idx, sim in enumerate(similarity_fifu.similarity("Berlin", 40)):
+    for idx, sim in enumerate(similarity_fifu.word_similarity("Berlin", 40)):
         assert SIMILARITY_ORDER[idx] == sim.word
 
 
 def test_similarity_stuttgart_10(similarity_fifu):
-    for idx, sim in enumerate(similarity_fifu.similarity("Stuttgart", 10)):
+    for idx, sim in enumerate(similarity_fifu.word_similarity("Stuttgart", 10)):
         assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
+
+
+def test_embedding_similarity_stuttgart_10(similarity_fifu):
+    stuttgart = similarity_fifu.embedding("Stuttgart")
+    sims = similarity_fifu.embedding_similarity(stuttgart, limit=10)
+    assert sims[0].word == "Stuttgart"
+
+    for idx, sim in enumerate(sims[1:]):
+        assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
+
+    for idx, sim in enumerate(similarity_fifu.embedding_similarity(stuttgart, skip={"Stuttgart"}, limit=10)):
+        assert SIMILARITY_ORDER_STUTTGART_10[idx] == sim.word
+
+
+def test_embedding_similarity_incompatible_shapes(similarity_fifu):
+    incompatible_embed = numpy.ones(1, dtype=numpy.float32)
+    with pytest.raises(ValueError):
+        similarity_fifu.embedding_similarity(incompatible_embed)
